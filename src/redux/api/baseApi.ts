e978fd12d904f516ece2b386f58d51a444fe9039
Tausif Ahmed <120933222+AR-Tausif@users.noreply.github.com>
2024-06-08 07:07:36 +0600

    Update baseApi.ts

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 // Define a service using a base URL and expected endpoints
-export const baseURL = "https://educa-website-three.vercel.app/api/v1";
+export const baseURL = "https://educa-international-school-server.vercel.app/api/v1";
 // export const baseURL = "http://localhost:5000/api/v1";
 //  export const baseURL = "https://educa-international-school-server.vercel.app/api/v1";
 
